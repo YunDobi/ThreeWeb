911983cc982fb77b3327cc42d7c5dc7a1b98ff81
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-03-15 20:11:53 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+import { NavBar} from './components/NavBar';
+import { Hero } from './components/Hero';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <NavBar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
