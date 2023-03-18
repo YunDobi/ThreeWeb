ece25a8544735548146e5a2e6a1664daf4165122
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-03-17 20:29:06 -0700

    clean the code first

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import { NavBar} from './components/NavBar';
 import { Hero } from './components/Hero';
-// import Computer from './components/Computer';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
           <NavBar />
           <Hero />
         </div>
-          {/* <Computer /> */}
       </div>
     </BrowserRouter>
   );
