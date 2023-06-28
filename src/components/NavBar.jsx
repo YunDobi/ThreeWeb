6542f16bdf0b5dcd3c5a821651cc2dedcba524f4
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-06-28 11:45:59 -0700

    quick check

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,35 +8,32 @@ import { logo, menu, close } from '../assets/';
 export const NavBar = () => {
   const [active, setActive] = useState('');
   const [toggle, setToggle] = useState(false);
-
   return (
     <nav
-      className={`${styles.paddingX} w-full flex item-center py-2 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className='flex justify-between max-w-7x1 mx-auto w-full items-center'>
+      <div className='flex justify-between max-w-7x1 mx-auto w-full'>
         <Link
           to={'/'}
-          className='flex item-center gap-2'
+          className='flex item-center gap'
           onClick={() => {
             setActive('');
             window.scrollTo(0, 0);
           }}
         >
-          <img
-            src={logo}
-            alt='logo'
-            className='w-[54px] h-[54px] object-contain'
-          />
-            <p className='text-white text-[18px] font-bold cursor-pointer flex items-center'>
-              Yuncheol Lee
-            </p>
-          {/* <div>
-            <span className=' text-white text-[18px] font-bold cursor-pointer sm:block hidden'>
-              Full Stack Developer
+          <img src={logo} alt='logo' className='w-[54px] h-[54px] object-contain' style={{marginRight: "8px"}} />
+          <div>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+            Yuncheol
+          </p>
+            <span
+              className=' text-white text-[18px] font-bold cursor-pointer sm:block hidden'
+            >AutoCAD Technician
             </span>
-          </div> */}
+
+          </div>
         </Link>
-        <ul className='list-none hidden sm:flex flex-row gap-[40px]'>
+        <ul className='list-none hidden sm:flex flex-row'>
           {navLinks.map((link) => (
             <li
               key={link.id}
@@ -44,6 +41,7 @@ export const NavBar = () => {
                 active === link.title ? 'text-white' : 'text-secondary'
               } hover:text-white cursor-pointer text-[18px] font-medium`}
               onClick={() => setActive(link.title)}
+              style={{margin:"10px 20px"}}
             >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
@@ -63,7 +61,7 @@ export const NavBar = () => {
               !toggle ? 'hidden' : 'flex'
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-x1`}
           >
-            <ul className='list-none flex justify-end items-start flex-col gap-[16px]'>
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
               {navLinks.map((link) => (
                 <li
                   key={link.id}
